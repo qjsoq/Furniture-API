663bfa44ed03aa97fb73554a2e15b53f0844827d
Dima Artamonov <artamonovdima355@gmail.com>
2024-03-30 15:49:31 +0300

    initial commit

---
+++ src/main/java/com/april/furnitureapi/FurnitureApiApplication.java
@@ -0,0 +1,13 @@
+package com.april.furnitureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurnitureApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/april/furnitureapi/FurnitureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.april.furnitureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
