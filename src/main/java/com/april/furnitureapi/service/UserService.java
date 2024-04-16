887253f783b7f62b2e570abf139f5cff5879ba66
Dima Artamonov <artamonovdima355@gmail.com>
2024-04-16 14:35:39 +0300

    Update service
    Create new method for finding user by email

--- src/main/java/com/april/furnitureapi/service/UserService.java
+++ src/main/java/com/april/furnitureapi/service/UserService.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserService {
     User signUp(User user);
     Optional<DecodedJWT> signIn(String email, String password);
+    User findByEmail(String email);
 }
--- src/main/java/com/april/furnitureapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/april/furnitureapi/service/impl/UserServiceImpl.java
@@ -44,5 +44,11 @@ public class UserServiceImpl implements UserService {
         return jwtTokenProvider.decodedJwt(token);
     }
 
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("dsa"));
+    }
+
 
 }
