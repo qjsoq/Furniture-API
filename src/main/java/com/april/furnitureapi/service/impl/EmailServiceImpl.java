f6309ace028fbb4558f4b0916bf32921430c3451
Dima Artamonov <artamonovdima355@gmail.com>
2024-06-17 15:38:46 +0300

    Update email sender

--- src/main/java/com/april/furnitureapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/april/furnitureapi/service/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.april.furnitureapi.service.impl;
 import com.april.furnitureapi.domain.User;
 import com.april.furnitureapi.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender mailSender;
+    @Value("${EMAIL_USERNAME}")
+    private String fromMail;
     @Override
     public void sendVerificationEmail(User user) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setSubject("Verify your new account");
         message.setTo(user.getEmail());
-        message.setFrom("kevinkenobi270@gmail.com");
+        message.setFrom(fromMail);
         message.setText("Yeees, it works");
         mailSender.send(message);
     }
