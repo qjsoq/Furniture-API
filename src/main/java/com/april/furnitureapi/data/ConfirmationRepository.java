1ec74e0ba8dca06f0a69f4b3ef6912770ac6d5e7
Dima Artamonov <artamonovdima355@gmail.com>
2024-06-18 14:05:27 +0300

    Implement confirmation functionality

---
+++ src/main/java/com/april/furnitureapi/data/ConfirmationRepository.java
@@ -0,0 +1,9 @@
+package com.april.furnitureapi.data;
+
+import com.april.furnitureapi.domain.Confirmation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ConfirmationRepository extends JpaRepository<Confirmation, UUID> {
+}
