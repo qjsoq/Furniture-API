2d5b1ef40eca534954d489b1ef7908667b67a4bf
Dima Artamonov <artamonovdima355@gmail.com>
2024-07-08 18:51:34 +0300

    Update the furniture services to retrieve furniture

--- src/main/java/com/april/furnitureapi/service/FurnitureService.java
+++ src/main/java/com/april/furnitureapi/service/FurnitureService.java
@@ -2,6 +2,10 @@ package com.april.furnitureapi.service;
 
 import com.april.furnitureapi.domain.Furniture;
 
+import java.util.*;
+
 public interface FurnitureService {
     Furniture saveFurniture(Furniture furniture, String email);
+    List<Furniture> findAll();
+    Furniture findByVendorCode(String vendorCode);
 }
--- src/main/java/com/april/furnitureapi/service/impl/FurnitureServiceImpl.java
+++ src/main/java/com/april/furnitureapi/service/impl/FurnitureServiceImpl.java
@@ -3,10 +3,15 @@ package com.april.furnitureapi.service.impl;
 import com.april.furnitureapi.data.FurnitureRepository;
 import com.april.furnitureapi.data.UserRepository;
 import com.april.furnitureapi.domain.Furniture;
+import com.april.furnitureapi.exception.VendorCodeAlreadyExists;
 import com.april.furnitureapi.service.FurnitureService;
 import com.april.furnitureapi.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +21,20 @@ public class FurnitureServiceImpl implements FurnitureService {
     @Override
     public Furniture saveFurniture(Furniture furniture, String email) {
         furniture.setCreator(userService.findByEmail(email));
+        furniture.setVendorCode(String.valueOf(new Random().nextInt(9999999 - 1000000 + 1) + 1000000));
+        if(furnitureRepository.existsByVendorCode(furniture.getVendorCode())){
+            throw new VendorCodeAlreadyExists("The random vendor code %s has already been used, try to save the furniture one more time.".formatted(furniture.getVendorCode()));
+        }
         return furnitureRepository.save(furniture);
+    }
+
+    @Override
+    public List<Furniture> findAll() {
+        return furnitureRepository.findAll();
+    }
+
+    @Override
+    public Furniture findByVendorCode(String vendorCode) {
+        return furnitureRepository.findByVendorCode(vendorCode);
     }
 }
