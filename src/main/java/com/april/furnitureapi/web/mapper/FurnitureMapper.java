35f3fe06fdec39d174515f9fc51da30242bd377d
Dima Artamonov <artamonovdima355@gmail.com>
2024-07-08 18:47:55 +0300

    Add furniture detailed DTO

--- src/main/java/com/april/furnitureapi/web/mapper/FurnitureMapper.java
+++ src/main/java/com/april/furnitureapi/web/mapper/FurnitureMapper.java
@@ -2,6 +2,7 @@ package com.april.furnitureapi.web.mapper;
 
 import com.april.furnitureapi.domain.Furniture;
 import com.april.furnitureapi.web.dto.furniture.FurnitureCreationDto;
+import com.april.furnitureapi.web.dto.furniture.FurnitureDetailedDto;
 import com.april.furnitureapi.web.dto.furniture.FurnitureDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,4 +13,5 @@ public interface FurnitureMapper {
     @Mapping(target = "price", expression = "java(Long.parseLong(furnitureCreationDto.getPrice()))")
     Furniture funitureCreationToFurniture(FurnitureCreationDto furnitureCreationDto);
     FurnitureDto furnitureToFurnitureDto(Furniture furniture);
+    FurnitureDetailedDto furnitureToDetailedDto(Furniture furniture);
 }
