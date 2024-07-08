3b92e094092219ffd430072887edfc47a8772084
Dima Artamonov <artamonovdima355@gmail.com>
2024-07-08 14:59:57 +0300

    Update the furniture DTO for creation

--- src/main/java/com/april/furnitureapi/web/dto/furniture/FurnitureCreationDto.java
+++ src/main/java/com/april/furnitureapi/web/dto/furniture/FurnitureCreationDto.java
@@ -1,5 +1,6 @@
 package com.april.furnitureapi.web.dto.furniture;
 
+import com.april.furnitureapi.utils.PriceConstraint;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,7 +13,8 @@ import org.hibernate.validator.constraints.Range;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FurnitureCreationDto {
     @Range(min = 0, max = 1000000, message = "The length of price should be between 1 and 10 digits")
-    Long price;
+    @PriceConstraint(message = "Price can contain only digits")
+    String price;
     @NotBlank(message = "Title cannot be blank")
     @Size(min = 3, max = 64, message = "Title of furniture cannot be less than 3 and more than 64 characters ")
     String title;
