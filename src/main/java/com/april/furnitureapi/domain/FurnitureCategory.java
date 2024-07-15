181211675347d16d4c1cfe2bae28a2eb42d56855
Dima Artamonov <artamonovdima355@gmail.com>
2024-07-15 15:39:55 +0300

    Implement exception handling for incorrect furniture categories

--- src/main/java/com/april/furnitureapi/domain/FurnitureCategory.java
+++ src/main/java/com/april/furnitureapi/domain/FurnitureCategory.java
@@ -1,7 +1,27 @@
 package com.april.furnitureapi.domain;
 
+import com.april.furnitureapi.exception.InvalidCategoryValueException;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.io.IOException;
+@JsonDeserialize(using = FurnitureCategory.FurnitureCategoryDeserializer.class)
 public enum FurnitureCategory {
     CHAIR, TABLE, COUCH,
     BED, ARMCHAIR, RECLINER,
-    ARMOIRE
+    ARMOIRE;
+    public static class FurnitureCategoryDeserializer extends JsonDeserializer<FurnitureCategory> {
+        @Override
+        public FurnitureCategory deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            String value = p.getText().toUpperCase();
+            try {
+                return FurnitureCategory.valueOf(value);
+            } catch (IllegalArgumentException e) {
+                throw new InvalidCategoryValueException("Invalid category: " + value);
+            }
+        }
+    }
 }
