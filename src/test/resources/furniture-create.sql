1831952c34ad9617b759f4600f5be6619116ec94
Dima Artamonov <artamonovdima355@gmail.com>
2024-08-31 20:02:01 +0300

    Add new SQL scripts for furniture and warehouse-storage creation

--- src/test/resources/furniture-create.sql
+++ src/test/resources/furniture-create.sql
@@ -1,7 +1,7 @@
 insert into furniture (id, title, price, availability, category, created_at, domain,
-                       vendor_code, creator_id)
-values (1, 'Taburetka', 7800, 'INSTOCK', 'CHAIR', now(), 'KITCHEN', '1235673', 7),
-       (2, 'Stol', 5450, 'INSTOCK', 'TABLE', now(), 'KITCHEN', '4238748', 7),
-       (3, 'Divan', 90000, 'INSTOCK', 'RECLINER', now(), 'LIVINGROOM', '4326746', 7),
-       (4, 'Shafa', 15000, 'INSTOCK', 'ARMOIRE', now(), 'BEDROOM', '9543422', 7),
-       (5, 'Krislo', 219787, 'OUTSTOCK', 'ARMCHAIR', now(), 'SITTINGROOM', '8563276', 7);
+                       vendor_code, creator_id, number_of_reviews)
+values (1, 'Taburetka', 7800, 'INSTOCK', 'CHAIR', now(), 'KITCHEN', '1235673', 7, 0),
+       (2, 'Stol', 5450, 'INSTOCK', 'TABLE', now(), 'KITCHEN', '4238748', 7, 0),
+       (3, 'Divan', 90000, 'INSTOCK', 'RECLINER', now(), 'LIVINGROOM', '4326746', 7, 0),
+       (4, 'Shafa', 15000, 'INSTOCK', 'ARMOIRE', now(), 'BEDROOM', '9543422', 7, 0),
+       (5, 'Krislo', 219787, 'OUTSTOCK', 'ARMCHAIR', now(), 'SITTINGROOM', '8563276', 7, 0);
